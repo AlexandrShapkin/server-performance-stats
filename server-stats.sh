fc1cf412dc67b1859c9ee2e95d2e6204487a8387
Alexandr Shapkin <shapkin.alexandr.sergeevich@gmail.com>
2026-06-25 06:12:16 +0700

    feat: memory usage statistics

--- server-stats.sh
+++ server-stats.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 CPU_USAGE="$(top -bn1 | grep 'Cpu(s)' | awk '{print 100 - $8}')"
+MEM_USAGE="$(free -m | awk '/Mem:/ { printf "Used: %.1f GB\nFree: %.1f GB\nUsage: %d%%%\n", $3/1024, $4/1024, $3/$4*100 }')"
 
 printf "========== SERVER PERFORMANCE STATS ==========\n\n"
 
-printf "CPU Usage: ${CPU_USAGE}%%\n"
+printf "CPU Usage: ${CPU_USAGE}%%\n\n"
+
+printf "Memory Usage:\n${MEM_USAGE}\n"
